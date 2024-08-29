9575643d815af9ebcd8c827705e7b00af956181f
Andzuo <alucasm02@gmail.com>
2024-08-29 20:15:57 -0300

    Feat: add TravelRoadMap

---
+++ src/main/java/com/server/turistai/entities/TravelRoadMap.java
@@ -0,0 +1,81 @@
+package com.server.turistai.entities;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "tb_travelRoadMap")
+public class TravelRoadMap {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String title;
+    private String comment;
+    private String addres;
+    private boolean visited;
+    @Column(name = "tb_travelRoadMap_images")
+    private List<String> images;
+
+    @ManyToOne
+    @JoinColumn(name = "travel_id", nullable = false)
+    private Travel travel;
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public Travel getTravel() {
+        return travel;
+    }
+
+    public void setTravel(Travel travel) {
+        this.travel = travel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getAddres() {
+        return addres;
+    }
+
+    public void setAddres(String addres) {
+        this.addres = addres;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+}
